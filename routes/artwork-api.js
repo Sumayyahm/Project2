7e5a1623f0dae596b81f31bd2b6ff4ca2820aaf4
Kathryn McDowell-Coston <kathrynmcdowell-coston@Kathryns-MacBook-Pro.local>
2020-10-17 13:55:12 -0500

    collection change to style

---
+++ routes/artwork-api.js
@@ -0,0 +1,16 @@
+// ******************************************************************************************************
+// artwork-api.js - this file offers a set of routes for displaying and saving data to the db for artwork
+// ******************************************************************************************************
+
+// Dependencies
+// =============================================================
+
+// Requiring our models
+var Artwork = require("../models/artwork.js");
+
+
+module.exports = function (app) {
+
+
+    
+}
