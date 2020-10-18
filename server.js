780085dc8ee2acc2099a228599dcf8e7afeecfa8
Cris Perez <cristino.perez525@gmail.com>
2020-10-18 17:03:59 -0500

    Corrected one DataTypes capitalization issue in the models folder, somewhat organized files to assist in fixing issue with css files being properly read over the server, updated some image and style references in the html files

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ require("./routes/html-routes.js")(app);
 
 db.sequelize.sync({ force: true }).then(function () {
   app.listen(PORT, function () {
-    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    console.log("==>  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
