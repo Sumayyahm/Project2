a2be75218ee515dace1fd557f2c3ec702e2ad079
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-18 14:22:18 -0500

    added express handlebars dependencies

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 var express = require("express");
 
 var app = express();
+var path = require("path");
 var PORT = process.env.PORT || process.env.LOCALPORT;
 
 var db = require("./models");
@@ -12,12 +13,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set('views', path.join(__dirname, '/views'));
+app.set("view engine", "handlebars");
+
 require("./routes/artist-api.js")(app);
 require("./routes/artwork-api.js")(app);
 require("./routes/customer-api.js")(app);
-
+require("./routes/style-api.js")(app);
 require("./routes/html-routes.js")(app);
-// require("./routes/api-routes.js")(app);
+
 
 db.sequelize.sync({ force: true }).then(function () {
 
