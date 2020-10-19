f2ccb4437222b7aa7521de93bb869fdc12e013c7
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-19 15:33:15 -0500

    Added routes for controllerfolder

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+var routes = require("./controller/artController.js");
+
+app.use(routes);
+
 
 require("./routes/artist-api.js")(app);
 require("./routes/artwork-api.js")(app);
