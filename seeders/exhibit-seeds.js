a7280f181657879c02fa91e20a4b5c59aa0f2ff4
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-10-20 15:20:09 -0500

    exhibit seeds and updats to style/exhibit models

---
+++ seeders/exhibit-seeds.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        return queryInterface.bulkInsert('Exhibit', [{
+
+            exhibit_name: "Impressionism: Reflections of Monet",
+            date: "January 20, 2021",
+            time: "8:30- 10:00 PM",
+            exhibit_descript: "Come see the works of Monet. Oscar-Claude Monet was a French painter, a founder of French Impressionist painting and the most consistent and prolific practitioner of the movement's philosophy of expressing one's perceptions before nature, especially as applied to plein air landscape painting",
+            exhibit_image: "",
+            exhibit_address: "6370 Timber Glen, Houston, Texas, 77084",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }, {
+            exhibit_name: "For Realism, isn't it Surreal?",
+            date: "January 27, 2021",
+            time: "8:30- 10:00 PM",
+            exhibit_descript: "",
+            exhibit_image: "",
+            exhibit_address: "6370 Timber Glen, Houston, Texas, 77084",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }, {
+            exhibit_name: "Lifes a Bunch of Palettes",
+            date: "February 3, 2021",
+            time: "8:30- 10:00 PM",
+            exhibit_descript: "",
+            exhibit_image: "",
+            exhibit_address: "6370 Timber Glen, Houston, Texas, 77084",
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }]);
+    },
+
+    down: async (queryInterface, Sequelize) => {
+
+    }
+};
