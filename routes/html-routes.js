a4bcc74100c382d9f8fae5de59d16b2e022599a6
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-21 09:47:04 -0500

    Added routes to all the pages

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -27,7 +27,7 @@ module.exports = function (app) {
 
 
                 res.render("index", headerData)
-                console.log(headerData.navArtists.Artist)
+                // console.log(headerData.navArtists.Artist)
             })
 
         })
@@ -36,7 +36,19 @@ module.exports = function (app) {
 
     });
 
-    app.get("/collections/:style_name", function(req,res){
+    app.get("/collection/:style_name", function(req,res){
+        db.Artist.findAll({}).then(function (dbArtist) {        
+
+            var artistObj = {
+                Artist: dbArtist
+            }
+
+            db.Style.findAll({}).then(function(dbStyle){
+
+                var styleObjNav ={
+                    Style:dbStyle
+                }
+       
         db.Style.findOne({
             where: {
                 style_name: req.params.style_name
@@ -46,6 +58,7 @@ module.exports = function (app) {
             var styleObj = {
                 Style: styleData
             }
+
             db.Artwork.findAll({
                 where: {
                     style_name: req.params.style_name
@@ -58,17 +71,118 @@ module.exports = function (app) {
 
                 var collectionsData = {
                     collArtwork:artworkObj,
-                    collStyle: styleObj
+                    collStyle: styleObj,
+                    navStyles:styleObjNav,
+                    navArtists: artistObj
                 }
                 
     
-                res.render("collections", collectionsData)
-                console.log(collectionsData)
+                res.render("collections", collectionsData);
+                console.log(collectionsData.collArtwork.Artwork);
             })
         })
-        
-
     })
+})
+})
+
+    app.get("/artist/:artistName", function(req,res) {
+        db.Artist.findAll({}).then(function (dbArtist) {        
+
+            var artistObj = {
+                Artist: dbArtist
+            }
+
+            db.Style.findAll({}).then(function(dbStyle){
+
+                var styleObjNav ={
+                    Style:dbStyle
+                }
+
+                db.Artist.findOne({
+                    where: {
+                        artist_name: req.params.artistName
+                    }
+                }).then(function(artistName){
+                    var artistDispName ={
+                        artistname: artistName
+                    }
+             
+                db.Artwork.findAll({
+                    where: {
+                        artist_name: req.params.artistName
+                    }
+                }).then(function(artworkData){
+                    
+                    var artworkObj ={
+                        Artwork: artworkData
+                    }
+
+                    var artistData = {
+                        artistArtwork:artworkObj,
+                        artistHeader: artistDispName,
+                        navStyles:styleObjNav,
+                        navArtists: artistObj
+                    }
+
+                    res.render("artists", artistData);
+                    console.log(artistData.artistHeader)
+                })
+            })
+         })
+      })
+    })
+
+    app.get("/exhibitions", function (req, res) {
+
+        db.Artist.findAll({}).then(function (dbArtist) {        
+
+            var artistObj = {
+                Artist: dbArtist
+            }
+
+            db.Style.findAll({}).then(function(dbStyle){
+
+                var styleObj ={
+                    Style:dbStyle
+                }
+    
+                var headerData = {
+                    navStyles:styleObj,
+                    navArtists: artistObj
+                }
+
+
+                res.render("exhibition", headerData)
+                // console.log(headerData.navArtists.Artist)
+            })
+        })  
+    });
+
+    app.get("/about", function (req, res) {
+
+        db.Artist.findAll({}).then(function (dbArtist) {        
+
+            var artistObj = {
+                Artist: dbArtist
+            }
+
+            db.Style.findAll({}).then(function(dbStyle){
+
+                var styleObj ={
+                    Style:dbStyle
+                }
+    
+                var headerData = {
+                    navStyles:styleObj,
+                    navArtists: artistObj
+                }
+
+
+                res.render("about", headerData)
+                // console.log(headerData.navArtists.Artist)
+            })
+        })  
+    });
 
     app.get("/manager", isAuthenticated, function (req, res) {
 
