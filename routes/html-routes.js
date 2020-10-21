c8280b99bd281389f2463f05d80e605e137d7705
Cris Perez <cristino.perez525@gmail.com>
2020-10-20 21:51:11 -0500

    finished most of the handlebar coding for collections page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,17 +8,12 @@ module.exports = function (app) {
 
     app.get("/", function (req, res) {
 
-
-
-        db.Artist.findAll({}).then(function (dbArtist) {
-
-            
+        db.Artist.findAll({}).then(function (dbArtist) {        
 
             var artistObj = {
                 Artist: dbArtist
             }
 
-            
             db.Style.findAll({}).then(function(dbStyle){
 
                 var styleObj ={
@@ -40,6 +35,40 @@ module.exports = function (app) {
         
 
     });
+
+    app.get("/collections/:style_name", function(req,res){
+        db.Style.findOne({
+            where: {
+                style_name: req.params.style_name
+            }
+        }).then(function(styleData){
+
+            var styleObj = {
+                Style: styleData
+            }
+            db.Artwork.findAll({
+                where: {
+                    style_name: req.params.style_name
+                }
+            }).then(function(artworkData){
+                
+                var artworkObj ={
+                    Artwork: artworkData
+                }
+
+                var collectionsData = {
+                    collArtwork:artworkObj,
+                    collStyle: styleObj
+                }
+                
+    
+                res.render("collections", collectionsData)
+                console.log(collectionsData)
+            })
+        })
+        
+
+    })
 
     app.get("/manager", isAuthenticated, function (req, res) {
 
