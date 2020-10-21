fb371f6a84d3f5ba41be51f43e789fef6f34b45d
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-21 09:52:19 -0500

    added href to listeners and deleted ajax func

--- public/js/script.js
+++ public/js/script.js
@@ -1,32 +1,15 @@
-var db = require("../../models")
-
 
 $(function() {
     $(".collection").on("click", function(event) {
         var styleName = $(this).data("target");
+        console.log(styleName);
         window.location.href= "/collection/" + styleName
-        // console.log(styleName);
-        // event.preventDefault();
-       
-        $.ajax("/api/style/" + styleName, {
-            type: "GET",
-            data: {StyleName:styleName
-        });
-        
     });
 
     $(".artist").on("click", function(event) {
         var artistName= $(this).data("name");
-        window.location.href= "views/artists.handlebars"
-        var Artist = {
-          artistname: artistName
-        }
-
-        $.ajax("/api/artwork/" + artistName, {
-            type: "GET",
-            data: Artist
-        });
-    })
+        window.location.href= "/artist/" + artistName
+    });
 
 
 
