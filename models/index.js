f82a7434179bf4f8df3af35ff85efbe187a650fa
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-10-21 23:34:35 -0500

    changed index.js

--- models/index.js
+++ models/index.js
@@ -8,8 +8,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+if (process.env.JAWSDB_URL) {
+  var sequelize = new Sequelize(process.env.JAWSDB_URL, {});
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
